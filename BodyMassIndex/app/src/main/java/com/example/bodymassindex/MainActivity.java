67534b3fa8ca33f59ff1ce057ad0ee0d116db995
Supanat <supanat.tan@dome.tu.ac.th>
2025-09-25 22:32:35 +0700

    add 8 digits and 2 decimal points check logic for edittext

--- BodyMassIndex/app/src/main/java/com/example/bodymassindex/MainActivity.java
+++ BodyMassIndex/app/src/main/java/com/example/bodymassindex/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.bodymassindex;
 
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText edit_text = (EditText)findViewById(R.id.editText_weight);
+        edit_text.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(8, 2)});
+
+    }
+}
+
+class DecimalDigitsInputFilter implements InputFilter {
+    private Pattern mPattern;
+    DecimalDigitsInputFilter(int digits, int digitsAfterZero) {
+        mPattern = Pattern.compile("[0-9]{0," + (digits - 1) + "}+((\\.[0-9]{0," + (digitsAfterZero - 1) +
+
+            "})?)||(\\.)?");
+    }
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        Matcher matcher = mPattern.matcher(dest);
+        if (!matcher.matches())
+            return "";
+        return null;
     }
 }
